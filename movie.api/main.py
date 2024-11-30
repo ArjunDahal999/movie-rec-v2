a42450a1f3ba128e00b082143ec51fdc19d41c7e
ArzunDhz <dahalarjun404@gmail.com>
2024-11-30 22:43:08 +0545

    oooko

--- movie.api/main.py
+++ movie.api/main.py
@@ -131,7 +131,7 @@ def predict_movies(movie: str, top_n: int = 15):
 
 @app.get('/')
 def home():
-    return {"status": "9999999df999 "}
+    return {"status": "s "}
 
 @app.get('/predict/{moviename}')
 def predict_movies_endpoint(moviename: str):
