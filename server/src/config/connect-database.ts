5215e161a0fa574cb175ecffa176f39dd0297356
ArzunDhz <dahalarjun404@gmail.com>
2024-12-03 22:08:16 +0545

    fix:server

--- server/src/config/connect-database.ts
+++ server/src/config/connect-database.ts
@@ -1,16 +1,14 @@
+import mongoose from "mongoose";
+import env from "../utils/validate-ENV";
 
-import mongoose from 'mongoose'
-import env from '../utils/validate-ENV'
-
-export const conntectToDatabase = async () =>
-{
-    mongoose.connect(`${env.MONGO_URL}`, { dbName: "MovieRecommendation" }).then(() =>
-    {
-        console.log('DATABASE CONNECTED!')
-    }).catch((err: any) =>
-    {
-        console.log(err)
-        console.log("failed to connect the database")
+export const conntectToDatabase = async () => {
+  mongoose
+    .connect(`${env.MONGO_URL}`, { dbName: "MovieRecommendation" })
+    .then(() => {
+      console.log("DATABASE CONNECTED!!!");
     })
-
-}
+    .catch((err: any) => {
+      console.log(err);
+      console.log("failed to connect the database");
+    });
+};
