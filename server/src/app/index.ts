4fbbbe2dac3bf6282ca303cc61f3f2edf27723ef
ArzunDhz <dahalarjun404@gmail.com>
2024-12-03 23:40:46 +0545

    server

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -58,7 +58,7 @@ app.use("/api/v1", allRouter);
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Server is workingss " });
+  res.json({ message: "Server is working" });
 });
 
 // Error middleware
