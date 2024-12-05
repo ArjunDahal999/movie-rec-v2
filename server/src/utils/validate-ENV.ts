ac915bf9202168ea25c9d047050aee071f55724a
ArzunDhz <dahalarjun404@gmail.com>
2024-12-05 19:54:34 +0545

    fix

--- server/src/utils/validate-ENV.ts
+++ server/src/utils/validate-ENV.ts
@@ -5,27 +5,27 @@ import path from "path";
 const envFilePath = path.resolve(path.dirname(__dirname), "../.env");
 
 dotenv.config({ path: envFilePath });
-export default cleanEnv(process.env, {
-  PORT: port(),
-  MONGO_URL: str(),
-  SMTP_HOST: str(),
-  SMTP_PORT: str(),
-  SMTP_MAIL: str(),
-  SMTP_PASSWORD: str(),
-  JWT_REFRESH_TOKEN_SECRET: str(),
-  JWT_ACCESS_TOKEN_SECRET: str(),
-});
+// export default cleanEnv(process.env, {
+//   PORT: port(),
+//   MONGO_URL: str(),
+//   SMTP_HOST: str(),
+//   SMTP_PORT: str(),
+//   SMTP_MAIL: str(),
+//   SMTP_PASSWORD: str(),
+//   JWT_REFRESH_TOKEN_SECRET: str(),
+//   JWT_ACCESS_TOKEN_SECRET: str(),
+// });
 
-// const env = {
-//   PORT: 4000,
-//   MONGO_URL:
-//     "mongodb+srv://admin-arjun:9pJ9D3YiqtYwXipM@cluster0.ldmnk.mongodb.net/?retryWrites=true&w=majority",
-//   SMTP_HOST: "smtp.gmail.com",
-//   SMTP_PORT: "465",
-//   SMTP_MAIL: "dahalarjun409@gmail.com",
-//   SMTP_PASSWORD: "xafbbasoqenxxqye",
-//   JWT_ACCESS_TOKEN_SECRET: "fasdfasdfa",
-//   JWT_REFRESH_TOKEN_SECRET: "afsdfadf",
-// };
+const env = {
+  PORT: 4000,
+  MONGO_URL:
+    "mongodb+srv://admin-arjun:9pJ9D3YiqtYwXipM@cluster0.ldmnk.mongodb.net/?retryWrites=true&w=majority",
+  SMTP_HOST: "smtp.gmail.com",
+  SMTP_PORT: "465",
+  SMTP_MAIL: "dahalarjun409@gmail.com",
+  SMTP_PASSWORD: "xafbbasoqenxxqye",
+  JWT_ACCESS_TOKEN_SECRET: "fasdfasdfa",
+  JWT_REFRESH_TOKEN_SECRET: "afsdfadf",
+};
 
-// export default env;
+export default env;
