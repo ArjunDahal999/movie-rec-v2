afb8fc1b8ad9281b23a35e6582b5074efbb17205
ArzunDhz <dahalarjun404@gmail.com>
2024-12-06 18:00:53 +0545

    doe

--- server/src/libs/sendMailLibs/send-email.ts
+++ server/src/libs/sendMailLibs/send-email.ts
@@ -1,39 +1,34 @@
-
 import nodemailer, { Transporter } from "nodemailer";
 import ejs from "ejs";
 import path from "path";
-import env from '../../utils/validate-ENV'
+import env from "../../utils/validate-ENV";
 
-export interface Emailoptions
-{
-    email: string;
-    subject: string;
-    templateName: string;
-    data?: { [key: string]: any };
+export interface Emailoptions {
+  email: string;
+  subject: string;
+  templateName: string;
+  data?: { [key: string]: any };
 }
 
-export const sendEmail = async (options: Emailoptions): Promise<void> =>
-{
+export const sendEmail = async (options: Emailoptions): Promise<void> => {
+  const transporter: Transporter = nodemailer.createTransport({
+    host: env.SMTP_HOST,
+    port: parseInt(env.SMTP_PORT || "587"),
+    auth: {
+      user: env.SMTP_MAIL,
+      pass: env.SMTP_PASSWORD,
+    },
+  });
 
-    const transporter: Transporter = nodemailer.createTransport({
-        host: env.SMTP_HOST,
-        port: parseInt(env.SMTP_PORT || "587"),
-        auth: {
-            user: env.SMTP_MAIL,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    });
+  const { email, data, subject, templateName } = options;
+  const templetePath = path.join(__dirname, "../../../src/mails", templateName);
+  const html = await ejs.renderFile(templetePath, data);
 
-    const { email, data, subject, templateName } = options;
-    const templetePath = path.join(__dirname, "../../../src/mails", templateName);
-    const html = await ejs.renderFile(templetePath, data);
-
-    const mailoptions = {
-        from: env.SMTP_MAIL,
-        to: email,
-        subject,
-        html,
-    };
-    await transporter.sendMail(mailoptions);
-
-}
+  const mailoptions = {
+    from: env.SMTP_MAIL,
+    to: email,
+    subject,
+    html,
+  };
+  await transporter.sendMail(mailoptions);
+};
